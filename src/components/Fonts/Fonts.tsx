af6268d13fe0f64dc34f0ce1e51710adf25d9ae0
marcoduarte21 <marco.duarte@ucr.ac.cr>
2025-04-12 19:10:59 -0600

    changes

--- src/components/Fonts/Fonts.tsx
+++ src/components/Fonts/Fonts.tsx
@@ -3,3 +3,15 @@ import { IProps } from "../../types/IProps";
 export const H1 = ({ styles, children }: IProps) => {
   return <div style={styles}>{children}</div>;
 };
+
+export const H2 = ({ styles, children }: IProps) => {
+  return <div style={styles}>{children}</div>;
+};
+
+export const H3 = ({ styles, children }: IProps) => {
+  return <div style={styles}>{children}</div>;
+};
+
+export const H4 = ({ styles, children }: IProps) => {
+  return <div style={styles}>{children}</div>;
+};
